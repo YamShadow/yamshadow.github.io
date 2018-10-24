d5d128238001658d8808e08044a050d2db40595c
2alheure <jojo77.juv@live.fr>
2018-10-24 21:18:44 +0200

    Train 24 10

--- tourner_la_page/script.js
+++ tourner_la_page/script.js
@@ -36,7 +36,43 @@
 var lastSeen = 'none';
 
 
+/**
+ * On va commencer par stocker les pages dans une variable ici.
+ * 
+ * Puis on reliera à une API par la suite.
+ * 
+ * On retient aussi ici le livre actuel, qui sera lui aussi plus tard donné par API
+ */
+var api = [
+    {
+        "nom": "SAO manga",
+        "couv": "img/SAO/couv.png",
+        "pages": [
+            "img/SAO/001.png",
+            "img/SAO/002.png",
+            "img/SAO/003.png",
+            "img/SAO/004.png"
+        ]
+    },
+    {
+        "nom": "SAO light novel",
+        "couv": "img/SAO_ln/couv.png",
+        "pages": [
+            "img/SAO_ln/001.png",
+            "img/SAO_ln/002.png",
+            "img/SAO_ln/003.png",
+            "img/SAO_ln/004.png"
+        ]
+    }
+];
 
+var actual = {
+    "book": api[1],
+    "page": {
+        "begin": 2,
+        "end": 3
+    }
+};
 
 // Ici, emit prend une string
 // Qui correspond à la fois au nom de l'event
@@ -46,11 +82,9 @@ AFRAME.registerComponent('emit', {
     init: function () {
         var eventToEmit = this.data;
         document.getElementById(eventToEmit).emit(eventToEmit);
+        console.log(eventToEmit);
     }
 });
-/* 
-Voir pour emettre un seul event, duquel on récupèrerait e pour switch son id
-*/
 
 /**
  * On écoute les différents events
@@ -58,29 +92,30 @@ Voir pour emettre un seul event, duquel on récupèrerait e pour switch son id
  * pour savoir quelle page afficher à présent.
  * Puis on update sa valeur.
 */
-document.addEventListener('marker-L', handleStart);
-document.addEventListener('marker-I', handleStart);
-document.addEventListener('marker-V', handleMiddle);
-document.addEventListener('marker-R', handleMiddle);
-document.addEventListener('marker-E', handleEnd);
-document.addEventListener('marker-S', handleEnd);
+document.addEventListener('start', (e) => handleStart('marker-L', 'marker-I'));
+document.addEventListener('middle', (e) => handleMiddle('marker-V', 'marker-R'));
+document.addEventListener('end', (e) => handleEnd('marker-E', 'marker-S'));
 
-// TODO : Remplacer par <img>
-
-function handleStart (event) {
-    if (lastSeen == 'start') event.preventDefault();
+/**
+ * Les fonctions de gestion des événements de pop in
+ */
+function handleStart (marker0, marker1) {
+    if (lastSeen == 'start') return;
     else {
         switch (lastSeen) {
             case 'end': 
-                document.getElementById(event.realTarget.id).setAttribute('text', 'value', nextPage()); 
+                nextPage();
+                updateMarkers(marker0, marker1);
                 break;
             case 'middle': 
-                document.getElementById(event.realTarget.id).setAttribute('text', 'value', prevPage()); 
+                prevPage();
+                updateMarkers(marker0, marker1);
                 break;
             case 'none':
             case 'couv':
             case 'menu':
-                document.getElementById(event.realTarget.id).setAttribute('text', 'value', actualPage()); 
+                actualPage();
+                updateMarkers(marker0, marker1);
                 break;
             default: 
                 console.error('Error while looking for last seen pages. Last seen is none of start, middle or end.');
@@ -88,4 +123,99 @@ function handleStart (event) {
         }
         lastSeen = 'start';
     }
+}
+
+function handleMiddle (marker0, marker1) {
+    if (lastSeen == 'middle') return;
+    else {
+        switch (lastSeen) {
+            case 'start': 
+                nextPage();
+                updateMarkers(marker0, marker1);
+                break;
+            case 'end': 
+                prevPage();
+                updateMarkers(marker0, marker1);
+                break;
+            case 'none':
+            case 'couv':
+            case 'menu':
+                actualPage();
+                updateMarkers(marker0, marker1);
+                break;
+            default: 
+                console.error('Error while looking for last seen pages. Last seen is none of middle, middle or end.');
+                console.error('Last seen is: '+lastSeen);
+        }
+        lastSeen = 'middle';
+    }
+}
+
+function handleEnd (marker0, marker1) {
+    if (lastSeen == 'end') return;
+    else {
+        switch (lastSeen) {
+            case 'middle': 
+                nextPage();
+                updateMarkers(marker0, marker1);
+                break;
+            case 'start': 
+                prevPage();
+                updateMarkers(marker0, marker1);
+                break;
+            case 'none':
+            case 'couv':
+            case 'menu':
+                updateMarkers(marker0, marker1);
+                break;
+            default: 
+                console.error('Error while looking for last seen pages. Last seen is none of end, middle or end.');
+                console.error('Last seen is: '+lastSeen);
+        }
+        lastSeen = 'end';
+    }
+}
+
+
+/**
+ * Les fonctions qui feront la liaison avec l'API,
+ * pour donner les pages à afficher
+ */
+
+function nextPage() {
+    actual = {
+        "book": actual.book,
+        "page": {
+            "begin": (actual.page.begin < actual.book.pages.length-3)? actual.page.begin+2 : actual.page.begin,
+            "end": (actual.page.end < actual.book.pages.length-3)? actual.page.end+2 : actual.page.end
+        }
+    };
+
+    return actual;
+}
+
+function prevPage() {
+    actual = {
+        "book": actual.book,
+        "page": {
+            "begin": (actual.page.begin>1)? actual.page.begin-2 : actual.page.begin,
+            "end": (actual.page.end>1)? actual.page.end-2 : actual.page.end
+        }
+    };
+
+    return actual;
+}
+
+function actualPage() {
+    return actual;
+}
+
+/**
+ * Fonction qui update l'affichage sur les marqueurs dont les id sont passés en paramètres.
+ * 
+ * Il switch le type des marqueurs (couv, ...) et va chercher actual pour les infos.
+ */
+function updateMarkers(marker0, marker1) {
+    document.getElementById(marker0).setAttribute('image', 'src', actual.book.pages[actual.page.begin]); 
+    document.getElementById(marker1).setAttribute('image', 'src', actual.book.pages[actual.page.end]); 
 }
